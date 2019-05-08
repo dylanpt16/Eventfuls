65d4338fa47f6cb0b055c271aecba8d3254e19e5
Dylan Chan <dptrinh1@gmail.com>
2019-05-08 12:47:32 -0700

    Minor fix: change video source

--- frontend/components/video/video.jsx
+++ frontend/components/video/video.jsx
@@ -4,7 +4,7 @@ class Video extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      root: 'http://res.cloudinary.com/dylanpt16/video/upload/v1517613759/video_c1yvd3.mp4'
+      root: 'http://res.cloudinary.com/dylanpt16/video/upload/v1520973975/eventfuls/video.webm'
     }
   }
 
@@ -16,7 +16,7 @@ class Video extends React.Component {
           autoPlay="true"
           poster="http://res.cloudinary.com/dylanpt16/image/upload/v1517614425/eventfuls/1_iW7i51bEZSnoPwGgWW93Kg.jpg"
           className="fullscreen-bg__video">
-          <source src={this.state.root} type="video/mp4" />
+          <source src={this.state.root} type="video/webm" />
         </video>
       </div>
     );
