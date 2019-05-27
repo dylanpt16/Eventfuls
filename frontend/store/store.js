3f62b4b07995541ac7fc81dcebfedc2f5e6bf0d2
Dylan Chan <dylanchan@Phuongs-MacBook-Pro.local>
2019-05-26 21:45:33 -0700

    Delete development tools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,18 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { compose } from 'redux';
-
 import rootReducer from '../reducers/root_reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => (
-  createStore(
-    rootReducer,
-    preloadedState,
-    composeEnhancers(applyMiddleware(thunk, logger))
-  )
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,35 @@
 var path = require("path");
 var webpack = require("webpack");
 
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+// include plugins config
 module.exports = {
   context: __dirname,
   entry: "./frontend/eventfuls.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
-  plugins:[
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
-    })
-  ],
+  plugins: plugins,
   module: {
     loaders: [
       {
@@ -37,3 +47,4 @@ module.exports = {
     extensions: [".js", ".jsx", "*"]
   }
 };
+
